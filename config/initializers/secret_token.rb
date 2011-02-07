86d7f09bb02291d4688812fcce7b42d699be51af
Fabian Becker <halfdan@xnorfz.de>
2011-02-07 21:26:22 +0100

    Initial Rails project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sodium::Application.config.secret_token = 'fd7d16204124fb9b1a33c663942c233ca05fd7332a6c0e48460f7ffd02d17399f6de16ea3d7846e0ad0fa74344d5c37e161574be8179bd464eb22987a3123595'
