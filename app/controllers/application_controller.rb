6bf73860bc21d9b5e8487f79f58485ecf033e676
Fabian Müller <fmueller83@googlemail.com>
2011-02-11 18:35:00 +0100

    applied support for i18n created subfolders in config/locale for models and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+
+  protected
+  def set_locale
+    I18n.locale = session[:locale]
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
+require 'i18n/backend/fallbacks'
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+
 
 # Initialize the rails application
 Sodium::Application.initialize!
