538126d7d72e08ce7614e7fac8475351b6dca301
Fabian Müller <fmueller83@googlemail.com>
2011-04-29 23:12:50 +0200

    added some routes for the static content pages.
    changed the root route to pages/index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Sodium::Application.routes.draw do
+#  get "pages/contact"
+#  get "pages/privacy"
+#  get "pages/about"
+#  get "pages/help"
+#  get "pages/tos"
+  get "pages/index"
+  get "timeline/index"
+
   devise_scope :user do
     get "login",  :to => "devise/sessions#new"
     get "logout", :to => "devise/sessions#destroy"
@@ -21,5 +29,5 @@ Sodium::Application.routes.draw do
   match "unfollow_project" => "projects#unfollow", :as => 'unfollow_project'
   match "projects/edit_member" => "projects#edit_member"
 
-  root :to => "timeline#index"
+  root :to => "pages#index"
 end
