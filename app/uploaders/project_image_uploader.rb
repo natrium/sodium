5fd9b8410571603168accdf14ead5b9ff9bc16af
halfdan <halfdan@xnorfz.de>
2011-05-04 21:09:14 +0200

    ProjectImageUploader now resizes images even if they are smaller than the given width/height.

--- app/uploaders/project_image_uploader.rb
+++ app/uploaders/project_image_uploader.rb
@@ -30,7 +30,7 @@ class ProjectImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-     process :resize_to_limit => [150, 150]
+     process :resize_to_fill => [150, 150]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
